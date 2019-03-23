c4abdfe45bcf364cfa123f059bbc4edaff77e7fb
Michael <ko4erov@mail.ru>
2019-03-23 23:27:47 +0300

    first commit

---
+++ js/main.js
@@ -0,0 +1,16 @@
+// $(document).ready(function(){
+$(function(){
+	$('#btn-open-form').click(function(e){
+		e.preventDefault();
+		$(this).hide();
+		$('.booking-form-wrapper').slideDown(1000);
+	})
+
+	$(".phone-mask").mask("+7 (999) 999-99-99");
+
+	var user = detect.parse(navigator.userAgent);
+	console.log(
+	  user.browser.family
+	);
+
+});
